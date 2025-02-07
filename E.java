6ae80cedad8c7044a164091a5a200076fcc8e7ba
Laxman Pajai <113297980+LaxmanPajai@users.noreply.github.com>
2025-02-07 17:03:55 +0530

    Update E.java

--- E.java
+++ E.java
@@ -7,16 +7,16 @@ public class E {
 	    public static void main(String[] args) {
 	        int count = 0;
 
-	        // Using for loop
+	        
 	        System.out.println("Using for loop:");
 	        for (int i = 111; i <= 212; i++) {
-	            if (i % 2 == 0) {  // Check if number is even
+	            if (i % 2 == 0) { 
 	                count++;
 	            }
 	        }
-	        System.out.println("Total even numbers: " + count + "\n");
+	        System.out.println("Total even numbers: " + count + "");
 
-	        // Using while loop
+	       
 	        System.out.println("Using while loop:");
 	        count = 0;
 	        int j = 111;
@@ -26,9 +26,9 @@ public class E {
 	            }
 	            j++;
 	        }
-	        System.out.println("Total even numbers: " + count + "\n");
+	        System.out.println("Total even numbers: " + count + "");
 
-	        // Using do-while loop
+	        
 	        System.out.println("Using do-while loop:");
 	        count = 0;
 	        int k = 111;
