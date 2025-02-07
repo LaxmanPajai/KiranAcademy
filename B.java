6f9aa51c66298d304b12744842f2709db8a7cf32
Laxman Pajai <113297980+LaxmanPajai@users.noreply.github.com>
2025-02-07 16:54:30 +0530

    Update B.java

--- B.java
+++ B.java
@@ -5,23 +5,23 @@ package loops;
 public class B {
 	
 	    public static void main(String[] args) {
-	        // Using for loop
+	       
 	        System.out.println("Using for loop:");
 	        for (int i = 44; i >= 22; i--) {
-	            System.out.print(i + " ");
+	            System.out.print( " ");
 	        }
 	        System.out.println("\n");
 
-	        // Using while loop
+	       
 	        System.out.println("Using while loop:");
 	        int j = 44;
 	        while (j >= 22) {
-	            System.out.print(j + " ");
+	            System.out.print( " ");
 	            j--;
 	        }
-	        System.out.println("\n");
+	        System.out.println("");
 
-	        // Using do-while loop
+	      
 	        System.out.println("Using do-while loop:");
 	        int k = 44;
 	        do {
