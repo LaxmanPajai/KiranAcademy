0e158e33d44983d8ea740405120de66ee100e771
Laxman Pajai <113297980+LaxmanPajai@users.noreply.github.com>
2025-02-07 17:05:44 +0530

    Update G.java

--- G.java
+++ G.java
@@ -4,14 +4,14 @@ package loops;
 public class G {
 	
 	    public static void main(String[] args) {
-	        // Using for loop
+	       
 	        System.out.println("Using for loop:");
 	        for (int i = 10; i <= 44; i++) {
 	            System.out.println("Square of " + i + " = " + (i * i));
 	        }
 	        System.out.println();
 
-	        // Using while loop
+	       
 	        System.out.println("Using while loop:");
 	        int j = 10;
 	        while (j <= 44) {
@@ -20,7 +20,7 @@ public class G {
 	        }
 	        System.out.println();
 
-	        // Using do-while loop
+	    
 	        System.out.println("Using do-while loop:");
 	        int k = 10;
 	        do {
