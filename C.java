65f6a09c74f443e497dd57924597f8971b86fd99
Laxman Pajai <113297980+LaxmanPajai@users.noreply.github.com>
2025-02-07 16:56:42 +0530

    Update C.java

--- C.java
+++ C.java
@@ -7,14 +7,14 @@ public class C {
 	
 	
 	    public static void main(String[] args) {
-	        // Using for loop
+	        
 	        System.out.println("Using for loop:");
 	        for (int i = 55; i <= 88; i += 3) {
 	            System.out.print(i + " ");
 	        }
 	        System.out.println("\n");
 
-	        // Using while loop
+	      
 	        System.out.println("Using while loop:");
 	        int j = 55;
 	        while (j <= 88) {
@@ -23,7 +23,7 @@ public class C {
 	        }
 	        System.out.println("\n");
 
-	        // Using do-while loop
+	    
 	        System.out.println("Using do-while loop:");
 	        int k = 55;
 	        do {
