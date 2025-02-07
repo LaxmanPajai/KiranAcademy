bfbeca54093d82c9a31613e35937f53be2ebcd07
Laxman Pajai <113297980+LaxmanPajai@users.noreply.github.com>
2025-02-07 17:06:21 +0530

    Update H.java

--- H.java
+++ H.java
@@ -5,14 +5,14 @@ package loops;
 public class H {
 	
 	    public static void main(String[] args) {
-	        // Using for loop
+	        
 	        System.out.println("Using for loop:");
 	        for (int i = 51; i >= 21; i--) {
 	            System.out.println("Cube of " + i + " = " + (i * i * i));
 	        }
 	        System.out.println();
 
-	        // Using while loop
+	        
 	        System.out.println("Using while loop:");
 	        int j = 51;
 	        while (j >= 21) {
@@ -21,7 +21,7 @@ public class H {
 	        }
 	        System.out.println();
 
-	        // Using do-while loop
+	      
 	        System.out.println("Using do-while loop:");
 	        int k = 51;
 	        do {
